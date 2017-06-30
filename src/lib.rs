298ec5a356e63f861d6454f3e328cc1aa56eb70d
cheezgi <zackh@firemail.cc>
2017-06-29 20:20:00 -0400

    add pet/pet interface

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+
+#![feature(vec_remove_item)]
+
+mod pet;
+pub use pet::*;
+
+use std::io;
+use std::io::Write;
+
+pub fn get_input() -> String {
+    io::stdout().flush().expect("Could not flush stdout");
+
+    let mut s = String::new();
+    let handle = io::stdin();
+
+    handle.read_line(&mut s).expect("Could not read line");
+
+    s.trim().to_owned()
+}
+
+trait Clamp {
+    fn clamp(self, upper: Self, lower: Self) -> Self;
+}
+
+impl<T> Clamp for T where T: std::cmp::Ord {
+    fn clamp(self, lower: T, upper: T) -> T {
+        if self > upper {
+            upper
+        } else if self < lower {
+            lower
+        } else {
+            self
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use Clamp;
+
+    #[test]
+    fn clamp_between_returns_self() {
+        let i = 50;
+        assert_eq!(50, i.clamp(0, 100));
+    }
+
+    #[test]
+    fn clamp_above_returns_upper() {
+        let i = 150;
+        assert_eq!(100, i.clamp(0, 100));
+    }
+
+    #[test]
+    fn clamp_below_returns_lower() {
+        let i = -150;
+        assert_eq!(0, i.clamp(0, 100));
+    }
+}
+
---
+++ src/pet.rs
@@ -0,0 +1,98 @@
+
+use Clamp;
+
+pub struct Pet {
+    dead: bool,
+    health: u8,
+    happiness: u8,
+    hunger: u8,
+    age: u8,
+    cleanliness: u8,
+    kind: String,
+    name: String,
+    inventory: Vec<String>,
+}
+
+impl Pet {
+    pub fn new(kind: String, name: String) -> Pet {
+        Pet {
+            dead: false,
+            health: 100,
+            happiness: 70,
+            hunger: 70,
+            age: 1,
+            cleanliness: 70,
+            kind: kind,
+            name: name,
+            inventory: vec![],
+        }
+    }
+
+    pub fn kind(&self) -> &str {
+        &self.kind
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn is_dead(&self) -> bool {
+        self.dead
+    }
+
+    pub fn give(&mut self, item: String) {
+        self.inventory.push(item);
+    }
+
+    pub fn take(&mut self, item: String) {
+        if self.inventory.contains(&item) {
+            self.inventory.remove_item(&item);
+        }
+    }
+
+    pub fn feed(&mut self, amt: u8) {
+        self.hunger += amt;
+        self.hunger.clamp(0, 100);
+    }
+
+    pub fn behunger(&mut self, amt: u8) {
+        self.hunger -= amt;
+        self.hunger.clamp(0, 100);
+    }
+
+    pub fn heal(&mut self, amt: u8) {
+        self.health += amt;
+        self.health.clamp(0, 100);
+    }
+
+    pub fn damage(&mut self, amt: u8) {
+        self.health -= amt;
+        self.health.clamp(0, 100);
+    }
+
+    pub fn clean(&mut self, amt: u8) {
+        self.cleanliness += amt;
+        self.cleanliness.clamp(0, 100);
+    }
+
+    pub fn dirty(&mut self, amt: u8) {
+        self.cleanliness -= amt;
+        self.cleanliness.clamp(0, 100);
+    }
+
+    pub fn play(&mut self, amt: u8) {
+        self.happiness += amt;
+        self.happiness.clamp(0, 100);
+    }
+
+    pub fn bore(&mut self, amt: u8) {
+        self.happiness -= amt;
+        self.happiness.clamp(0, 100);
+    }
+
+    pub fn age(&mut self, amt: u8) {
+        self.age += amt;
+        self.age.clamp(0, 20);
+    }
+}
+
