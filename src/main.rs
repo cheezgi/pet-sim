0342cfc2a60a70cd1d60c0f7374bec3f8bbb68ef
cheezgi <zackh@firemail.cc>
2017-06-29 20:22:10 -0400

    start main interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+extern crate pet_sim;
+
 fn main() {
     println!("Welcome to Pet Simulator!");
+
+    print!("What kind of pet do you have? ");
+    let kind = pet_sim::get_input();
+
+    print!("What's its name? ");
+    let name = pet_sim::get_input();
+
+    let mut pet = pet_sim::Pet::new(kind, name);
+
+    pet.age(1);
+
+    println!("{} the {}, aww!", pet.name(), pet.kind());
 }
+
