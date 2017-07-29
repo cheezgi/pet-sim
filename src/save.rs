06b13392a329f2d819aa2f55b6e5b3dafe88272b
cheezgi <zackh@firemail.cc>
2017-07-29 14:21:10 -0400

    make fields public

--- src/save.rs
+++ src/save.rs
@@ -1,25 +1,25 @@
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Copy)]
 pub struct Settings {
-    feed_scale: u8,
-    hunger_scale: u8,
-    heal_scale: u8,
-    damage_scale: u8,
-    clean_scale: u8,
-    dirty_scale: u8,
-    play_scale: u8,
-    bore_scale: u8,
+    pub feed_scale: u8,
+    pub hunger_scale: u8,
+    pub heal_scale: u8,
+    pub damage_scale: u8,
+    pub clean_scale: u8,
+    pub dirty_scale: u8,
+    pub play_scale: u8,
+    pub bore_scale: u8,
 
-    hunger_damage: u8,
-    hunger_sicken: u8,
-    cleanliness_sicken: u8,
-    health_kill: u8,
-    age_kill: u8,
+    pub hunger_damage: u8,
+    pub hunger_sicken: u8,
+    pub cleanliness_sicken: u8,
+    pub health_kill: u8,
+    pub age_kill: u8,
 
-    health_message: u8,
-    hunger_message: u8,
-    bore_message: u8,
-    clean_message: u8,
+    pub health_message: u8,
+    pub hunger_message: u8,
+    pub bore_message: u8,
+    pub clean_message: u8,
 }
 
 // TODO: fill, add ser/de
