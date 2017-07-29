59d07bf79d0678183891645581302b7d5694d6d2
cheezgi <zackh@firemail.cc>
2017-07-29 13:27:41 -0400

    move money operations to concrete values

--- src/player.rs
+++ src/player.rs
@@ -1,15 +1,10 @@
 
 use item::Item;
-use amt::Amount;
-use ::{sub_scale, add_scale};
 
 pub struct Player {
     money: i32,
     inventory: Vec<Item>,
     story: (), // TODO: add story progress marker
-
-    deposit_scale: u8,
-    withdraw_scale: u8,
 }
 
 impl Player {
@@ -18,9 +13,6 @@ impl Player {
             money: 500,
             inventory: vec![],
             story: (),
-
-            deposit_scale: 5,
-            withdraw_scale: 5,
         }
     }
 
@@ -28,16 +20,12 @@ impl Player {
         self.money
     }
 
-    pub fn deposit(&mut self, amt: Amount) -> i32 {
-        let r = amt.scale(self.deposit_scale) as i32;
-        self.money = self.money + r;
-        r
+    pub fn deposit(&mut self, amt: i32) {
+        self.money += amt;
     }
 
-    pub fn withdraw(&mut self, amt: Amount) -> i32 {
-        let r = amt.scale(self.withdraw_scale) as i32;
-        self.money = self.money - r;
-        r
+    pub fn withdraw(&mut self, amt: i32) {
+        self.money -= amt;
     }
 }
 
